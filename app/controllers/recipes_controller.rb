571f53d8bfff07aa5e7194dd121b2c2a833c8fe8
John Tuttle <thejwt@gmail.com>
2017-11-26 09:56:17 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,12 @@ class RecipesController < ApplicationController
   end
 
   def create
-
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to @ingredient
+    else
+      render 'new'
+    end
   end
 
 end
