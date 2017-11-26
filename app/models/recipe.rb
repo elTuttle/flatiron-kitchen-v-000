16ce466486bc39c617d83c588296312d2d80325d
John Tuttle <thejwt@gmail.com>
2017-11-26 09:47:14 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
-  has_many :recipe_ingredients
+  has_many :recipe_ingredients, class_name: RecipeIngredient
   has_many :ingredients, through: :recipe_ingredients
 
   validates_presence_of :name
