7b0f3e9fad450532fc46b8f62c38d04be9a7c03d
John Tuttle <thejwt@gmail.com>
2017-11-26 09:42:51 +0000

    automatically backed up by learn

--- db/migrate/20171126080725_create_recipe_ingredients.rb
+++ db/migrate/20171126080725_create_recipe_ingredients.rb
@@ -1,8 +1,9 @@
 class CreateRecipeIngredients < ActiveRecord::Migration
   def change
     create_table :recipe_ingredients do |t|
-      t.integer :recipe_id
-      t.integer :ingredient_id
+      t.belongs_to :recipe, index: true, foreign_key: true
+      t.belongs_to :category, index: true, foreign_key: true
+      t.timestamps null: false
     end
   end
 end
